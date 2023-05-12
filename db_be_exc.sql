78b3effa9b28f587603d85e4a7f9bd199277589a
Haqq, Muhammad Hendra Nailul <muhammad.hendra.haqq@accenture.com>
2023-05-12 14:30:44 +0700

    add db file

---
+++ db_be_exc.sql
@@ -0,0 +1,36 @@
+-- Adminer 4.8.1 MySQL 8.0.31 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `departments` (`id`, `name`) VALUES
+(1,	'Marketing'),
+(2,	'Customer Service');
+
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `department_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKgy4qe3dnqrm3ktd76sxp7n4c2` (`department_id`),
+  CONSTRAINT `FKgy4qe3dnqrm3ktd76sxp7n4c2` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `employees` (`id`, `email`, `first_name`, `last_name`, `department_id`) VALUES
+(3,	'john.doe@example.com',	'John',	'Doe Updated',	1);
+
+-- 2023-05-12 07:29:57
